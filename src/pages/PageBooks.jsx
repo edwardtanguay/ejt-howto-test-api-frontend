7a3e8ca2270a7928a8f850117120f87d83809d52
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-24 08:11:26 +0200

    spinner in

--- src/pages/PageBooks.jsx
+++ src/pages/PageBooks.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { FaSpinner } from 'react-icons/fa';
+import FadeIn from 'react-fade-in';
 
 // const url = 'http://localhost:4433';
 const url = 'https://ejt-howto-test-api.herokuapp.com';
@@ -8,28 +10,45 @@ export const PageBooks = () => {
 	const [books, setBooks] = useState([]);
 
 	useEffect(() => {
-		(async () => {
-			setBooks((await axios.get(url)).data);
-		})();
+		setTimeout(() => {
+			(async () => {
+				setBooks((await axios.get(url)).data);
+			})();
+		}, 0);
 	}, []);
 
 	return (
 		<>
 			<h2>Books</h2>
-			<p>There are {books.length} books.</p>
-			<div className="books">
-				{books.map((book, index) => {
-					return (
-						<div key={index} className="book">
-							<img src={`https://edwardtanguay.netlify.app/share/images/techBooks/${book.idCode}.jpg`} alt="book"/>
-							<div className="info">
-							<div className="title">{book.title}</div>
-							<div className="description">{book.description}</div>
-							</div>
-						</div>
-					);
-				})}
-			</div>
+			<FadeIn>
+				{books.length === 0 ? (
+					<div className="waitArea">
+						<FaSpinner className="spinner" />
+					</div>
+				) : (
+					<div className="books">
+						<p>There are {books.length} books.</p>
+						{books.map((book, index) => {
+							return (
+								<div key={index} className="book">
+									<img
+										src={`https://edwardtanguay.netlify.app/share/images/techBooks/${book.idCode}.jpg`}
+										alt="book"
+									/>
+									<div className="info">
+										<div className="title">
+											{book.title}
+										</div>
+										<div className="description">
+											{book.description}
+										</div>
+									</div>
+								</div>
+							);
+						})}
+					</div>
+				)}
+			</FadeIn>
 		</>
 	);
 };
